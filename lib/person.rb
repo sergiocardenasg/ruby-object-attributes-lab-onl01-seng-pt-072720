4276fa0b5a15bec4196871516eeb85cec2601af8
Sergio Cardenas <sergiocardenas@utexas.edu>
2020-08-11 01:05:32 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,9 @@
 class Person
 
-  def initialize(name, job)
-    @name = name
-    @job = job
-  end
+  # def initialize(name, job)
+  #   @name = name
+  #   @job = job
+  # end
 
   def name=(name)
     @name = name
