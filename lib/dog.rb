806950fcb12dc06256385b271306428241bae67c
Sergio Cardenas <sergiocardenas@utexas.edu>
2020-08-11 01:06:43 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+
+  # def initialize(dog_name, dog_breed)
+  #   @dog_name = dog_name
+  #   @dog_breed = dog_breed
+  # end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+end
