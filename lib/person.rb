a805929543d4f2c0fe0573c53536c8cf3fcf87d4
Sergio Cardenas <sergiocardenas@utexas.edu>
2020-08-11 01:04:05 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,25 @@
+class Person
+
+  def initialize(dog_name, dog_breed)
+    @dog_name = dog_name
+    @dog_breed = dog_breed
+  end
+
+  def name=(dog_name)
+    @dog_name = dog_name
+  end
+
+  def name
+    @dog_name
+  end
+
+  def breed=(dog_breed)
+    @dog_breed = dog_breed
+  end
+
+  def breed
+    @dog_breed
+  end
+end
+
+iker = Dog.new("Iker", "Schnauzer")
