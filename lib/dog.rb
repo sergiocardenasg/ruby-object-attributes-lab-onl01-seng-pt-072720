333500ea344601fca440a7a974ea64b3a19fc399
Sergio Cardenas <sergiocardenas@utexas.edu>
2020-08-11 01:05:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,9 @@
 class Dog
 
-  def initialize(dog_name, dog_breed)
-    @dog_name = dog_name
-    @dog_breed = dog_breed
-  end
+  # def initialize(dog_name, dog_breed)
+  #   @dog_name = dog_name
+  #   @dog_breed = dog_breed
+  # end
 
   def name=(dog_name)
     @dog_name = dog_name
